8e1ac43da5df06a949d894ed42eb3e6b7af043b8
ViktorDavydov <davydov.viktor.andreevich@gmail.com>
2023-08-19 14:25:53 +0300

    upper commit

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def upper_text(random_string):
+    """String upper function"""
     return random_string.upper()
+
+
+def capital_text(random_string2):
+    """String capitalize function"""
+    return random_string2.capitalize()
